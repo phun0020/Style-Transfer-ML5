77aa7b5cfe17f499c8f9387668b703a66451b1c2
Tam Phung <phun0020@algonquinlive.com>
2018-09-27 13:03:25 -0400

    add highlight to selected img

--- sketch.js
+++ sketch.js
@@ -8,13 +8,18 @@ function setup() {
 
 // events
 $('img.style-img').click((el) => {
-    let path = $(el)[0].target.src;
+    $('img.style-img').removeClass('selected-img');
+    let target = $(el)[0].target;
+    target.classList.add('selected-img');
+    let path = target.src;
     let styleName = getLastSegment(path);
     style = ml5.styleTransfer(`model/${styleName}`, modelLoaded);
 });
 
 $('img.input-img').click((el) => {
+    $('img.input-img').removeClass('selected-img');
     inputImg = $(el)[0].target;
+    inputImg.classList.add('selected-img');
     if(style != undefined) modelLoaded();
 });
 
